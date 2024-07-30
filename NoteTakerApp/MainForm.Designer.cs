8710eff9953a77bc9fb55ec66916943d07c4f82b
Bami O. <bamideleogundeji949@gmail.com>
2024-07-30 13:09:05 -0400

    Refactor MainForm UI and data handling
    Replaced `urgentRadioButton` with `urgencyCheckBox1` in `MainForm.Designer.cs`. Updated `DataGridView` controls to `toDoDataGridView` and `doneDataGridView`, and added `toDoLabel` and `doneLabel` for task counts. Adjusted layout and positioning of controls.
    
    In `MainForm.cs`, included `System.Data` namespace and added `toDoList` and `doneList` `DataTable` objects. Updated `Form1_Load` to initialize and bind these tables to the respective `DataGridView` controls.
    
    Removed obsolete metadata from `MainForm.resx` related to old `DataGridView` columns.

--- NoteTakerApp/MainForm.Designer.cs
+++ NoteTakerApp/MainForm.Designer.cs
@@ -33,25 +33,19 @@
             descriptionLabel = new Label();
             descTextBox1 = new TextBox();
             timeComboBox = new ComboBox();
-            urgentRadioButton = new RadioButton();
             publishButton = new Button();
-            dataGridView1 = new DataGridView();
-            taskColumn1 = new DataGridViewTextBoxColumn();
-            descColumn = new DataGridViewTextBoxColumn();
-            timeColumn = new DataGridViewTextBoxColumn();
-            urgentColumn = new DataGridViewTextBoxColumn();
-            dataGridView2 = new DataGridView();
-            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
-            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
-            dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
-            dataGridViewTextBoxColumn4 = new DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            toDoDataGridView = new DataGridView();
+            doneDataGridView = new DataGridView();
+            toDoLabel = new Label();
+            doneLabel = new Label();
+            urgencyCheckBox1 = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)toDoDataGridView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)doneDataGridView).BeginInit();
             SuspendLayout();
             // 
             // taskTextBox1
             // 
-            taskTextBox1.Location = new Point(23, 76);
+            taskTextBox1.Location = new Point(23, 59);
             taskTextBox1.Name = "taskTextBox1";
             taskTextBox1.Size = new Size(188, 31);
             taskTextBox1.TabIndex = 0;
@@ -59,7 +53,7 @@
             // taskLabel
             // 
             taskLabel.AutoSize = true;
-            taskLabel.Location = new Point(18, 38);
+            taskLabel.Location = new Point(18, 21);
             taskLabel.Name = "taskLabel";
             taskLabel.Size = new Size(45, 25);
             taskLabel.TabIndex = 1;
@@ -68,7 +62,7 @@
             // descriptionLabel
             // 
             descriptionLabel.AutoSize = true;
-            descriptionLabel.Location = new Point(17, 125);
+            descriptionLabel.Location = new Point(17, 107);
             descriptionLabel.Name = "descriptionLabel";
             descriptionLabel.Size = new Size(102, 25);
             descriptionLabel.TabIndex = 3;
@@ -76,135 +70,90 @@
             // 
             // descTextBox1
             // 
-            descTextBox1.Location = new Point(22, 163);
+            descTextBox1.Location = new Point(22, 145);
             descTextBox1.Multiline = true;
             descTextBox1.Name = "descTextBox1";
-            descTextBox1.Size = new Size(289, 91);
+            descTextBox1.Size = new Size(289, 71);
             descTextBox1.TabIndex = 2;
             // 
             // timeComboBox
             // 
             timeComboBox.FormattingEnabled = true;
             timeComboBox.Items.AddRange(new object[] { "Morning", "Midday", "Afternoon", "Evening", "Night" });
-            timeComboBox.Location = new Point(23, 273);
+            timeComboBox.Location = new Point(29, 245);
             timeComboBox.Name = "timeComboBox";
             timeComboBox.Size = new Size(182, 33);
             timeComboBox.TabIndex = 4;
             timeComboBox.Text = "Time of Day";
             // 
-            // urgentRadioButton
-            // 
-            urgentRadioButton.AutoSize = true;
-            urgentRadioButton.Location = new Point(22, 325);
-            urgentRadioButton.Name = "urgentRadioButton";
-            urgentRadioButton.Size = new Size(91, 29);
-            urgentRadioButton.TabIndex = 5;
-            urgentRadioButton.TabStop = true;
-            urgentRadioButton.Text = "Urgent";
-            urgentRadioButton.UseVisualStyleBackColor = true;
-            // 
             // publishButton
             // 
-            publishButton.Location = new Point(17, 391);
+            publishButton.Location = new Point(29, 391);
             publishButton.Name = "publishButton";
             publishButton.Size = new Size(159, 42);
             publishButton.TabIndex = 6;
             publishButton.Text = "Publish";
             publishButton.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // toDoDataGridView
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { taskColumn1, descColumn, timeColumn, urgentColumn });
-            dataGridView1.Location = new Point(432, 59);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(723, 143);
-            dataGridView1.TabIndex = 7;
+            toDoDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            toDoDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            toDoDataGridView.Location = new Point(432, 59);
+            toDoDataGridView.Name = "toDoDataGridView";
+            toDoDataGridView.RowHeadersWidth = 62;
+            toDoDataGridView.Size = new Size(723, 143);
+            toDoDataGridView.TabIndex = 7;
             // 
-            // taskColumn1
+            // doneDataGridView
             // 
-            taskColumn1.HeaderText = "Task";
-            taskColumn1.MinimumWidth = 8;
-            taskColumn1.Name = "taskColumn1";
-            taskColumn1.ReadOnly = true;
-            taskColumn1.Width = 150;
+            doneDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            doneDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            doneDataGridView.Location = new Point(432, 290);
+            doneDataGridView.Name = "doneDataGridView";
+            doneDataGridView.RowHeadersWidth = 62;
+            doneDataGridView.Size = new Size(723, 143);
+            doneDataGridView.TabIndex = 8;
             // 
-            // descColumn
+            // toDoLabel
             // 
-            descColumn.HeaderText = "Description";
-            descColumn.MinimumWidth = 8;
-            descColumn.Name = "descColumn";
-            descColumn.ReadOnly = true;
-            descColumn.Width = 150;
+            toDoLabel.AutoSize = true;
+            toDoLabel.Location = new Point(432, 21);
+            toDoLabel.Name = "toDoLabel";
+            toDoLabel.Size = new Size(121, 25);
+            toDoLabel.TabIndex = 9;
+            toDoLabel.Text = "To Be Done: 0";
             // 
-            // timeColumn
+            // doneLabel
             // 
-            timeColumn.HeaderText = "Time";
-            timeColumn.MinimumWidth = 8;
-            timeColumn.Name = "timeColumn";
-            timeColumn.ReadOnly = true;
-            timeColumn.Width = 150;
+            doneLabel.AutoSize = true;
+            doneLabel.Location = new Point(432, 245);
+            doneLabel.Name = "doneLabel";
+            doneLabel.Size = new Size(142, 25);
+            doneLabel.TabIndex = 10;
+            doneLabel.Text = "Tasks Finished: 0";
             // 
-            // urgentColumn
+            // urgencyCheckBox1
             // 
-            urgentColumn.HeaderText = "Urgent";
-            urgentColumn.MinimumWidth = 8;
-            urgentColumn.Name = "urgentColumn";
-            urgentColumn.ReadOnly = true;
-            urgentColumn.Width = 150;
-            // 
-            // dataGridView2
-            // 
-            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewTextBoxColumn3, dataGridViewTextBoxColumn4 });
-            dataGridView2.Location = new Point(432, 290);
-            dataGridView2.Name = "dataGridView2";
-            dataGridView2.RowHeadersWidth = 62;
-            dataGridView2.Size = new Size(723, 143);
-            dataGridView2.TabIndex = 8;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            dataGridViewTextBoxColumn1.HeaderText = "Task";
-            dataGridViewTextBoxColumn1.MinimumWidth = 8;
-            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            dataGridViewTextBoxColumn1.ReadOnly = true;
-            dataGridViewTextBoxColumn1.Width = 150;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            dataGridViewTextBoxColumn2.HeaderText = "Description";
-            dataGridViewTextBoxColumn2.MinimumWidth = 8;
-            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            dataGridViewTextBoxColumn2.ReadOnly = true;
-            dataGridViewTextBoxColumn2.Width = 150;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            dataGridViewTextBoxColumn3.HeaderText = "Time";
-            dataGridViewTextBoxColumn3.MinimumWidth = 8;
-            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            dataGridViewTextBoxColumn3.ReadOnly = true;
-            dataGridViewTextBoxColumn3.Width = 150;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            dataGridViewTextBoxColumn4.HeaderText = "Urgent";
-            dataGridViewTextBoxColumn4.MinimumWidth = 8;
-            dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            dataGridViewTextBoxColumn4.ReadOnly = true;
-            dataGridViewTextBoxColumn4.Width = 150;
+            urgencyCheckBox1.AutoSize = true;
+            urgencyCheckBox1.Location = new Point(29, 306);
+            urgencyCheckBox1.Name = "urgencyCheckBox1";
+            urgencyCheckBox1.Size = new Size(92, 29);
+            urgencyCheckBox1.TabIndex = 11;
+            urgencyCheckBox1.Text = "Urgent";
+            urgencyCheckBox1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1176, 445);
-            Controls.Add(dataGridView2);
-            Controls.Add(dataGridView1);
+            Controls.Add(urgencyCheckBox1);
+            Controls.Add(doneLabel);
+            Controls.Add(toDoLabel);
+            Controls.Add(doneDataGridView);
+            Controls.Add(toDoDataGridView);
             Controls.Add(publishButton);
-            Controls.Add(urgentRadioButton);
             Controls.Add(timeComboBox);
             Controls.Add(descriptionLabel);
             Controls.Add(descTextBox1);
@@ -213,8 +162,8 @@
             Name = "MainForm";
             Text = "To Do List";
             Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)toDoDataGridView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)doneDataGridView).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -226,17 +175,11 @@
         private Label descriptionLabel;
         private TextBox descTextBox1;
         private ComboBox timeComboBox;
-        private RadioButton urgentRadioButton;
         private Button publishButton;
-        private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn taskColumn1;
-        private DataGridViewTextBoxColumn descColumn;
-        private DataGridViewTextBoxColumn timeColumn;
-        private DataGridViewTextBoxColumn urgentColumn;
-        private DataGridView dataGridView2;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private DataGridView toDoDataGridView;
+        private DataGridView doneDataGridView;
+        private Label toDoLabel;
+        private Label doneLabel;
+        private CheckBox urgencyCheckBox1;
     }
 }
--- NoteTakerApp/MainForm.cs
+++ NoteTakerApp/MainForm.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace NoteTakerApp
 {
     public partial class MainForm : Form
@@ -7,9 +9,26 @@ namespace NoteTakerApp
             InitializeComponent();
         }
 
+        DataTable toDoList = new DataTable();
+        DataTable doneList = new DataTable();
+
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            //making columns for the to do list
+            toDoList.Columns.Add("Task");
+            toDoList.Columns.Add("Description");
+            toDoList.Columns.Add("Time");
+            toDoList.Columns.Add("Urgency");
 
+            //pointing the to do list to the data grid view
+            toDoDataGridView.DataSource = toDoList;
+
+            doneList.Columns.Add("Task");
+            doneList.Columns.Add("Description");
+
+
+            doneDataGridView.DataSource = doneList;
         }
     }
 }
